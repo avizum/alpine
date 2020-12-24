066a1d9958ab3a5d1a6dbf58a107975198afee37
avizum <71485362+jbkn@users.noreply.github.com>
2020-12-23 21:06:45 -0800

    asd

--- cogs/misc.py
+++ cogs/misc.py
@@ -116,7 +116,7 @@ class Miscellaneous(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def pull(self, ctx):
-        command = 'git clone git@github.com:jbkn/avimetry'
+        command = 'git pull origin main'
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
         print(output, error)
