ee59773f100612ca995dd453f225701411dac846
avizum <71485362+jbkn@users.noreply.github.com>
2020-12-27 22:09:44 -0800

    v56

--- avimetrybot/cogs/auto-responder.py
+++ avimetrybot/cogs/auto-responder.py
@@ -13,13 +13,44 @@ class AutoResponder(commands.Cog):
     #If Bot is pinged it responds with prefix
     @commands.Cog.listener()
     async def on_message(self, message):
+        with open("./avimetrybot/files/counting.json", "r") as f:
+            cc = json.load(f)
+            if str(message.guild.id) in cc:
+                if message.channel.name == "counting":
+                    if message.content != str(cc[str(message.guild.id)]):
+                        await message.delete()
+                    else:
+                        with open("./avimetrybot/files/counting.json", "r") as f:
+                            cc = json.load(f)
+                    
+                        cc[str(message.guild.id)] +=1
+                        with open("./avimetrybot/files/counting.json", "w") as f:
+                            json.dump(cc, f, indent=4)
+            else:
+
+                cc[str(message.guild.id)] = 0
+                with open("./avimetrybot/files/counting.json", "w") as f:
+                    json.dump(cc, f, indent=4)
+
         if message.author == self.avibot.user:
             return
+           
         if message.content == '<@!756257170521063444>':
             with open("./avimetrybot/files/prefixes.json", "r") as f:
                 prefixes = json.load(f)
             pre = prefixes[str(message.guild.id)]
             await message.channel.send(f"Hey, {message.author.mention}, my prefix is `{pre}`")
-        
+         
+
+            if message.content !=str(cc):
+                await message.delete()
+            else:
+                with open("./avimetrybot/files/counting.json", "r") as f:
+                    cc = json.load(f)
+                
+                cc[str(message.guild.id)] = 2
+                with open("./avimetrybot/files/counting.json", "w") as f:
+                    json.dump(cc, f, indent=4)
+            
 def setup(avibot):
     avibot.add_cog(AutoResponder(avibot))
