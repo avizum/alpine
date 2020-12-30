6fceb23ff5e1e7a6994a3b257c954f1e317cbfa3
avizum <71485362+jbkn@users.noreply.github.com>
2020-12-29 21:13:10 -0800

    v0.67a

--- avimetrybot/cogs/automod.py
+++ avimetrybot/cogs/automod.py
@@ -9,7 +9,36 @@ class AutoMod(commands.Cog):
     
     def __init__(self, avibot):
         self.avibot = avibot
+        self.mc = commands.CooldownMapping.from_cooldown(5, 8, commands.BucketType.member)
+        self.coold = commands.CooldownMapping.from_cooldown(0, 1, commands.BucketType.member)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.avibot.user:
+            return
+        if message.author.bot:
+            return
+        with open("./avimetrybot/files/badword.json", "r") as f:
+            blacklist = json.load(f)
+
+        for words in blacklist:
+            if words in message.content.lower():
+                await message.delete()
+                await message.channel.send(f"{message.author.mention}, don't say that word!", delete_after=3)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, message_before, message_after):
+        if message_before.author == self.avibot.user:
+            return
         
+        with open("./avimetrybot/files/badword.json", "r") as f:
+            blacklist = json.load(f)
+
+        for words in blacklist:
+            if words in message_after.content.lower():
+                await message_after.delete()
+                await message_after.channel.send(f"{message_after.author.mention}, don't edit your message to say that word!", delete_after=3)
+
 def setup(avibot):
     avibot.add_cog(AutoMod(avibot))
 
