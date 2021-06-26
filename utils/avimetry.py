6d4ad843645b84b7e6d4293f8ba1742152dcd7ae
avizum <71485362+avizum@users.noreply.github.com>
2021-06-26 01:46:47 -0700

    6.26r2

--- utils/avimetry.py
+++ utils/avimetry.py
@@ -172,7 +172,7 @@ class AvimetryBot(commands.Bot):
         self.session = aiohttp.ClientSession()
         self.pool = self.loop.run_until_complete(asyncpg.create_pool(**self.pg["postgresql"]))
         self.loop.create_task(self.cache.cache_all())
-        self.loop.create_task(self.initiate_obsidian())
+        # self.loop.create_task(self.initiate_obsidian())
 
         @self.check
         async def check(ctx):
