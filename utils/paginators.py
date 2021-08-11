e1231457f89f054de77d57590359e368e4a9ac96
avizum <juliusrt@outlook.com>
2021-08-11 00:12:17 -0700

    Change reaction layout in Paginators

--- utils/paginators.py
+++ utils/paginators.py
@@ -17,6 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from discord.ext import menus
+from discord.ext.menus import button, Position
 
 
 class AvimetryPages(menus.MenuPages):
@@ -38,3 +39,37 @@ class AvimetryPages(menus.MenuPages):
             elif payload.event_type == 'REACTION_REMOVE':
                 return
         await super().update(payload)
+
+    def _skip_double_triangle_buttons(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages <= 2
+
+    @button('\N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f', position=Position(0),
+            skip_if=_skip_double_triangle_buttons)
+    async def go_to_first_page(self, payload):
+        """go to the first page"""
+        await self.show_page(0)
+
+    @button('\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f', position=Position(1))
+    async def go_to_previous_page(self, payload):
+        """go to the previous page"""
+        await self.show_checked_page(self.current_page - 1)
+
+    @button('\N{BLACK SQUARE FOR STOP}\ufe0f', position=Position(2))
+    async def stop_pages(self, payload):
+        """stops the pagination session."""
+        self.stop()
+
+    @button('\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f', position=Position(3))
+    async def go_to_next_page(self, payload):
+        """go to the next page"""
+        await self.show_checked_page(self.current_page + 1)
+
+    @button('\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f', position=Position(4),
+            skip_if=_skip_double_triangle_buttons)
+    async def go_to_last_page(self, payload):
+        """go to the last page"""
+        # The call here is safe because it's guarded by skip_if
+        await self.show_page(self._source.get_max_pages() - 1)
