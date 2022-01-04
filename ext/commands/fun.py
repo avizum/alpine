0d6e87e2e879436e1dc0fa5cb7f21a2901f9fbd5
avizum <71485362+avizum@users.noreply.github.com>
2022-01-03 22:59:39 -0800

    Add some 8 ball responses

--- ext/commands/fun.py
+++ ext/commands/fun.py
@@ -51,27 +51,27 @@ class Fun(core.Cog):
         This command is not meant for actual advice.
         """
         responses = [
-            "As I see it, yes.", "Ask again later.",
-            "Better not tell you now.", "Cannot predict now.",
-            "Concentrate and ask again.", "Don't count on it.",
             "It is certain.", "It is decidedly so.",
-            "Most likely.", "My reply is no.",
-            "My sources say no.", "Outlook not so good.",
-            "Outlook good.", "Reply hazy, try again.",
-            "Signs point to yes.", "Very doubtful.",
-            "Without a doubt.", "Yes.",
-            "Yes - definitely.", "You may rely on it.",
+            "Without a doubt.", "Yes, definitely",
+            "You may rely on it", "As I see it, yes.",
+            "Most Likely.", "Outlook good."
+            "Yes.", "Signs point to yes.",
+            "Certainly.", "Very certain.",
+            "Absolutely.", "Definitely",
+            "Yeah."
+
+            "Reply hazy, try again.", "Ask again later.",
+            "Better tell you later", "Better not tell you now.",
+            "Can not predict now.", "Concentrate and ask again.",
+            "Go away." f"Try again {ctx.author.display_name}.",
+            "THINK HARDER AND TRY AGAIN", "You again?"
+
+            "No", "As I see it, no."
+            "Don't count on it.", "My reply is no.",
+            "No, definately not.", "My sources say no.",
+            "Outlook not so good.", "Very doubtful.",
+            "You have got to be kidding me.", "Oh God, NO."
         ]
-        if ctx.author.id in self.bot.owner_ids and question.lower().endswith(
-            "\u200b"
-        ):
-            responses = [
-                "It is certain.", "Without a doubt.",
-                "You may rely on it.", "Yes definitely.",
-                "It is decidedly so.", "As I see it, yes.",
-                "Most likely.", "Yes.",
-                "Outlook good.", "Signs point to yes.",
-            ]
         ballembed = discord.Embed(title=":8ball: Magic 8 Ball")
         ballembed.add_field(name="Question:", value=f"{question}", inline=False)
         ballembed.add_field(
