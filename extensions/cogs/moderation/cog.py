2f8bc0dee4dafeae96078e95925efead136df0b7
avizum <juliusrt@outlook.com>
2022-09-03 22:58:48 -0700

    Change `masskick` and `massban` to be `HybridCommand`

--- extensions/cogs/moderation/cog.py
+++ extensions/cogs/moderation/cog.py
@@ -80,9 +80,13 @@ class Moderation(core.Cog):
                 kick_embed.description = f"**{target}** has been kicked from the server, However, I could not DM them."
         await ctx.send(embed=kick_embed, ephemeral=True)
 
-    @core.command()
+    @core.command(hybrid=True)
     @core.has_permissions(ban_members=True)
     @core.bot_has_permissions(ban_members=True)
+    @core.describe(
+        targets="The people to kick. (Seperate with spaces.)",
+        reason="Reason that will show up in audit logs."
+    )
     async def masskick(
         self,
         ctx: Context,
@@ -96,18 +100,26 @@ class Moderation(core.Cog):
         You can not kick people with higher permissions than you.
         """
         if not targets:
-            return await ctx.send("One or more members can not be kick by you. Try again.")
+            return await ctx.send("One or more members can not be kick by you. Try again.", ephemeral=True)
         new_targets = ", ".join(str(i) for i in targets)
-        conf = await ctx.confirm(message=f"Do you want to kick {new_targets} ({len(targets)} members) with reason {reason}?")
+        conf = await ctx.confirm(
+            message=f"This will kick:\n{new_targets} ({len(targets)} members),\nWith the reason {reason}.",
+            confirm_messsage=f'Press "Yes" to kick {(len(targets))} members or "No" to cancel.',
+            ephemeral=True,
+            delete_message_after=True,
+        )
         if conf.result:
             fail = 0
-            m = await ctx.send("Kicking...")
+            await conf.message.edit(content="Kicking...")
             for member in targets:
                 try:
                     await member.kick(reason=reason)
                 except Exception:
                     fail += 1
-            await m.edit(content=f"Sucessfully kicked {len(targets)-fail}/{len(targets)} members.")
+            await conf.message.edit(content=f"Sucessfully kicked {len(targets)-fail}/{len(targets)} members.")
+        else:
+            await conf.message.edit(content="Cancelled.")
+
 
     @core.command(hybrid=True)
     @core.both_has_permissions(ban_members=True)
@@ -168,6 +180,10 @@ class Moderation(core.Cog):
     @core.command()
     @core.has_permissions(ban_members=True)
     @core.bot_has_permissions(ban_members=True)
+    @core.describe(
+        targets="The people to ban. (Seperate with spaces.)",
+        reason="Reason that will show up in audit logs."
+    )
     async def massban(
         self,
         ctx: Context,
@@ -181,18 +197,23 @@ class Moderation(core.Cog):
         You can not ban people with a role higher than you or higher permissions.
         """
         if not targets:
-            return await ctx.send("One or more members can not be banned by you. Try again.")
+            return await ctx.send("One or more members can not be banned by you. Try again.", ephemeral=True)
         new_targets = ", ".join(str(i) for i in targets)
-        conf = await ctx.confirm(message=f"Do you want to ban {new_targets} ({len(targets)} members) with reason {reason}?")
+        conf = await ctx.confirm(
+            message=f"This will ban:\n{new_targets} ({len(targets)} members),\nWith reason {reason}.",
+            confirm_messsage=f'Press "Yes" to ban {(len(targets))} members or "No" to cancel.',
+            ephemeral=True,
+            delete_message_after=True,
+        )
         if conf.result:
             fail = 0
-            m = await ctx.send("Banning...")
+            await conf.message.edit(content="Banning...")
             for member in targets:
                 try:
                     await member.ban(reason=reason)
                 except Exception:
                     fail += 1
-            await m.edit(content=f"Sucessfully banned {len(targets)-fail}/{len(targets)} members.")
+            await conf.message.edit(content=f"Sucessfully banned {len(targets)-fail}/{len(targets)} members.")
 
     @core.command(hybrid=True)
     @core.both_has_permissions(ban_members=True)
@@ -232,7 +253,7 @@ class Moderation(core.Cog):
         Temporarily mutes a member in the server.
 
         This uses the "Time Out" feature in discord.
-        The minumum mute time is 1 second and the maximum is 28 days. (Discord Limitation.)
+        The minimum mute time is 1 minute and the maximum is 28 days. (Discord Limitation.)
         You can not mute people that are higher than you in the role hierarchy.
         """
         if duration > 2419200 or duration < 60:
@@ -444,7 +465,15 @@ class Moderation(core.Cog):
         purged = await self.do_purge(ctx, limit=amount, check=check, before=ctx.message)
         await ctx.can_delete(embed=await self.do_affected(purged), ephemeral=True)
 
-    @core.command(hybrid=True)
+    @core.group(hybrid=True, invoke_without_command=True)
+    @core.both_has_permissions(manage_channels=True)
+    async def channel(self, ctx: Context):
+        """
+        Channel management commands.
+        """
+        await ctx.send_help(ctx.command)
+
+    @channel.command()
     @core.both_has_permissions(manage_messages=True)
     @core.default_permissions(manage_messages=True)
     @core.describe(channel="The channel to lock.", reason="The reason for locking the channel.")
@@ -474,7 +503,7 @@ class Moderation(core.Cog):
         if channel != ctx.channel:
             await channel.send(embed=lc)
 
-    @core.command(hybrid=True)
+    @channel.command()
     @core.both_has_permissions(manage_channels=True)
     @core.default_permissions(manage_channels=True)
     @core.describe(channel="The channel to unlock.", reason="The reason for unlocking the channel.")
@@ -504,30 +533,30 @@ class Moderation(core.Cog):
         if channel != ctx.channel:
             await channel.send(embed=lc)
 
-    @core.command()
-    @core.has_permissions(manage_channels=True)
-    @core.bot_has_permissions(manage_channels=True)
-    async def slowmode(self, ctx: Context, *, seconds: TimeConverter):
+    @channel.command()
+    @core.both_has_permissions(manage_channels=True)
+    @core.describe(duration="The duration to slowmode the channel for.")
+    async def slowmode(self, ctx: Context, *, duration: TimeConverter):
         """
         Set the channel's slowmode delay.
 
         The maximum slowmode you can set is 6 hours.
         Setting 0 seconds will disable slowmode.
         """
-        if seconds > 21600:
+        if duration > 21600:
             raise commands.BadArgument("Amount should be less than or equal to 6 hours")
         if isinstance(ctx.channel, discord.Thread):
             return await ctx.send("Slowmode delay cannot be set in threads.")
-        await ctx.channel.edit(slowmode_delay=seconds)  # type: ignore  # not sure why that happens
+        await ctx.channel.edit(slowmode_delay=duration)  # type: ignore  # not sure why that happens
         embed = discord.Embed(
             title="Changed Slowmode",
-            description=f"Slowmode delay has been set to {humanize.precisedelta(seconds)}",
+            description=f"Slowmode delay has been set to {humanize.precisedelta(duration)}",
         )
         await ctx.send(embed=embed)
 
-    @core.group(invoke_without_command=True)
-    @core.has_permissions(manage_roles=True)
-    @core.bot_has_permissions(manage_roles=True)
+    @core.group(hybrid=True, invoke_without_command=True)
+    @core.both_has_permissions(manage_roles=True)
+    @core.default_permissions(manage_roles=True)
     async def role(self, ctx: Context):
         """
         Add or remove a role from a member.
@@ -535,13 +564,18 @@ class Moderation(core.Cog):
         await ctx.send_help("role")
 
     @role.command(aliases=["append"])
-    @core.has_permissions(manage_roles=True)
-    async def add(self, ctx: Context, member: discord.Member, role: discord.Role):
+    @core.both_has_permissions(manage_roles=True)
+    @core.describe(member="The member's roles to modify.", role="The role to add to the member.")
+    async def add(self, ctx: Context, member: TargetMember, role: discord.Role):
         """
         Add a role to a member.
 
         If I can't add a role to them, I will give you a reason why.
         """
+        if role >= ctx.me.top_role:
+            raise commands.BadArgument("I can't add that role to them.")
+        elif role >= ctx.author.top_role:
+            raise commands.BadArgument("You can't add that role to them.")
         await member.add_roles(role)
         ra = discord.Embed(
             title="Added Role",
@@ -550,13 +584,18 @@ class Moderation(core.Cog):
         await ctx.send(embed=ra)
 
     @role.command()
-    @core.has_permissions(manage_roles=True)
-    async def remove(self, ctx: Context, member: discord.Member, role: discord.Role):
+    @core.both_has_permissions(manage_roles=True)
+    @core.describe(member="The member's roles to modify.", role="The role to add to the member.")
+    async def remove(self, ctx: Context, member: TargetMember, role: discord.Role):
         """
         Remove a role from a member.
 
         If I can't remove that role from them, I will give you a reason why.
         """
+        if role >= ctx.me.top_role:
+            raise commands.BadArgument("I can't remove that role to them.")
+        elif role >= ctx.author.top_role:
+            raise commands.BadArgument("You can't remove that role to them.")
         await member.remove_roles(role)
         rr = discord.Embed()
         rr.add_field(
@@ -565,8 +604,11 @@ class Moderation(core.Cog):
         )
         await ctx.send(embed=rr)
 
-    @core.command()
+    @core.command(hybrid=True)
     @core.has_permissions(kick_members=True)
+    @core.bot_has_permissions(manage_nicknames=True)
+    @core.default_permissions(kick_members=True)
+    @core.describe(member="The member to nick.", nick="The name to set.")
     async def nick(self, ctx: Context, member: TargetMember, *, nick: str | None = None):
         """
         Gives or chanes a person's nick name.
